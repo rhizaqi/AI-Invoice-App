5f02a7aa4b61330482750da3f2cd3b5deb400c80
Oki <rhizaqi94@gmail.com>
2025-10-03 20:47:52 +0700

    feat: done hero part for landing page

--- fe-invoice-generator/src/components/Landing/Hero.jsx
+++ fe-invoice-generator/src/components/Landing/Hero.jsx
@@ -9,30 +9,44 @@ export default function Hero() {
       <div className="absolute inset-0 bg-grid-white/[0.5] bg-[size:60px_60px]"></div>
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 lg:py-32">
         <div className="text-center max-w-4xl mx-auto">
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold text-blue-950 leading-tight mb-6">
+          <h1 className="text-3xl sm:text-5xl lg:text-6xl font-extrabold text-blue-950 leading-tight mb-6">
             Ai-Powered Invoicing, Made Effortless
           </h1>
-          <p className="text-xl sm:text-xl text-gray-700 mb-8 leading-relaxed max-w-3xl mx-auto">
+          <p className="text-base sm:text-xl text-gray-700 mb-8 leading-relaxed max-w-3xl mx-auto">
             Let our AI create invoice from simple text, generate payment
             reminders, and provide smart insights to help manage your finance.
           </p>
           <div className="flex flex-cols sm:flex-row items-center justify-center space-y-4 sm:space-y-0 sm:space-x-4">
             {isAuthenticated ? (
-              <Link to="/dashboard" className=""></Link>
+              <Link
+                to="/dashboard"
+                className="bg-gradient-to-r from-blue-950 to-blue-900 text-white px-8 py-4 rounded-xl font-bold text-base sm:text-lg hover:bg-blue-900 transition-all duration-200 hover:scale-105 hover:shadow-2xl transform"
+              >
+                Go to Dashboard
+              </Link>
             ) : (
-              <Link to="/signup" className="">
+              <Link
+                to="/signup"
+                className="bg-gradient-to-r from-blue-950 to-blue-900 text-white px-8 py-4 rounded-xl font-bold text-base sm:text-lg hover:bg-gray-800 transition-all duration-200 hover:scale-105 hover:shadow-2xl transform"
+              >
                 Get started for Free
               </Link>
             )}
-            <a href="#features" className="">
+            <a
+              href="#features"
+              className="border-2 border-black text-black px-8 py-4 rounded-xl font-bold text-base sm:text-lg hover:bg-white hover:text-black transition-all duration-200 hover:scale-105"
+            >
               Learn More
             </a>
           </div>
         </div>
       </div>
-
-      <div className="">
-        <img src={Front_Img} alt="Invoice App Screenshot" className="" />
+      <div className="mt-12 sm:mt-6 relative max-w-5xl mx-auto">
+        <img
+          src={Front_Img}
+          alt="Invoice App Screenshot"
+          className="rounded-2xl shadow-2xl shadow-gray-300 border-4 border-gray-200/20"
+        />
       </div>
     </section>
   );
