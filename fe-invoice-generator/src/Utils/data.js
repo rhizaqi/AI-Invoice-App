ebee11694e9a889a3380ee33e10fa2f86536df50
Oki <rhizaqi94@gmail.com>
2025-10-04 15:37:37 +0700

    feat: features and testimonials

---
+++ fe-invoice-generator/src/Utils/data.js
@@ -0,0 +1,52 @@
+import { BarChart2, FileText, Mail, Sparkles } from "lucide-react";
+
+export const FEATURES = [
+  {
+    icon: Sparkles,
+    title: "AI Invoice Creation",
+    description:
+      "Paste any text, email, or receipt and let our AI instantly generate a complete, profesional invoice for you",
+  },
+  {
+    icon: BarChart2,
+    title: "AI Powered Dashboard",
+    description:
+      "Get smart, actionable insights about your business finances, generated automatically by our AI analyst.",
+  },
+  {
+    icon: Mail,
+    title: "Smart Reminders",
+    description:
+      "Automatically generate polite and effective payment reminder emails for overdue invoices with single click.",
+  },
+  {
+    icon: FileText,
+    title: "Easy Invoice Management",
+    description:
+      "Easily manage all your invoices, track payments, and send reminders for overdue payments.",
+  },
+];
+
+export const TESTIMONIALS = [
+  {
+    quote:
+      "This app saved me hours of work. I can now create and send invoices in minutes!",
+    author: "Jane Doe",
+    title: "Freelance Designer",
+    avatar: "https://placehold.co/100x100/000000/ffffff?text=JD",
+  },
+  {
+    quote:
+      "The best invoicing app I have ever used. Simple, Intuitive, and Powerful.",
+    author: "John Smith",
+    title: "Small Owner Business",
+    avatar: "https://placehold.co/100x100/000000/ffffff?text=JS",
+  },
+  {
+    quote:
+      "I love the dashboard and reporting features. It helps me keeps track my finance.",
+    author: "Peter Jones",
+    title: "Consultant",
+    avatar: "https://placehold.co/100x100/000000/ffffff?text=PJ",
+  },
+];
--- fe-invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ fe-invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,17 @@
+import Features from "../../components/Landing/Features";
 import Header from "../../components/Landing/Header";
 import Hero from "../../components/Landing/Hero";
+import Testimonial from "../../components/Landing/Testimonial";
+
 
 export default function LandingPage() {
   return (
     <div className="bg-[#ffffff] text-gray-600">
       <Header />
-      <main>
-        <Hero/>
+      <main className="mb-[96]">
+        <Hero />
+        <Features />
+        <Testimonial/>
       </main>
     </div>
   );
