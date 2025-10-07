43231199bd29557c206959b55f2bc82ca3a14fc2
Oki <rhizaqi94@gmail.com>
2025-10-07 12:45:42 +0700

    feat: start do some backend

--- be-invoice-generator/app.js
+++ be-invoice-generator/app.js
@@ -1,1 +1,28 @@
-// disini app nya
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Middleware to handle CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["content-type", "Authorization"],
+  })
+);
+
+// connect Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes here
+
+// App start here
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
---
+++ be-invoice-generator/models/User.js
@@ -0,0 +1,53 @@
+// User Models
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      minlength: 6,
+      select: false,
+    },
+    businessName: {
+      type: String,
+      default: "",
+    },
+    address: {
+      type: String,
+      default: "",
+    },
+    phone: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+//password hashing middleware
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//method to compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
